cfba2c3d0e160e774388ff80b858d1266ba7a536
nahudev21 <nahueldelatorre16@gmail.com>
2024-07-28 18:23:08 -0300

    Subi el proyecto base

---
+++ src/main/java/com/Nahudev/products_service_apiRest/ProductsServiceApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.Nahudev.products_service_apiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsServiceApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsServiceApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Nahudev/products_service_apiRest/ProductsServiceApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nahudev.products_service_apiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServiceApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
